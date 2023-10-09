b69be6f8b21d6b4f690c8b7f028ded63a4788cf9
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-08 21:33:58 -0300

    initial commit
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ src/main/java/com/faesa/librarycli/LibrarycliApplication.java
@@ -0,0 +1,13 @@
+package com.faesa.librarycli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarycliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarycliApplication.class, args);
+    }
+
+}
