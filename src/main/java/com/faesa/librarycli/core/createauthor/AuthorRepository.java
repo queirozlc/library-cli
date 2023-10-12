2cf14a970539e41a4e4014c48da7f9dd72692e4e
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-12 14:39:46 -0300

    feat(createauthor): Add AuthorRepository and AuthorRepositoryImpl implementation
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/createauthor/AuthorRepository.java
+++ src/main/java/com/faesa/librarycli/core/createauthor/AuthorRepository.java
@@ -1,2 +1,8 @@
-package com.faesa.librarycli.core.createauthor;public interface AuthorRepository {
+package com.faesa.librarycli.core.createauthor;
+
+import com.faesa.librarycli.shared.infra.database.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends BaseRepository<Author, Long> {
 }
--- src/main/java/com/faesa/librarycli/core/createauthor/AuthorRepositoryImpl.java
+++ src/main/java/com/faesa/librarycli/core/createauthor/AuthorRepositoryImpl.java
@@ -1,2 +1,16 @@
-package com.faesa.librarycli.core.createauthor;public class AuthorRepositoryImpl {
+package com.faesa.librarycli.core.createauthor;
+
+import com.faesa.librarycli.shared.core.adapters.AbstractSimpleJDBCRepository;
+import com.faesa.librarycli.shared.core.ports.QueryBuilder;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.util.Set;
+
+
+@Component
+public class AuthorRepositoryImpl extends AbstractSimpleJDBCRepository<Author, Long> implements AuthorRepository {
+    public AuthorRepositoryImpl(Connection connection, Set<QueryBuilder> queryBuilders) {
+        super(connection, queryBuilders);
+    }
 }
