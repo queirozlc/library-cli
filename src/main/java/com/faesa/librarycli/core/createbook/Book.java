45e618ca8e063356a561473b2f1f49f13bd5e629
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-13 00:07:18 -0300

    [CreateBook] Adds functionality to create a new book in the library
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/createbook/Book.java
+++ src/main/java/com/faesa/librarycli/core/createbook/Book.java
@@ -1,2 +1,73 @@
-package com.faesa.librarycli.core.createbook;public class Book {
+package com.faesa.librarycli.core.createbook;
+
+import com.faesa.librarycli.core.createauthor.Author;
+import com.faesa.librarycli.shared.infra.database.DomainValuesExtractor;
+import lombok.Getter;
+import org.springframework.util.Assert;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Book implements DomainValuesExtractor<Long> {
+
+    @Getter
+    private Long id;
+    private String title;
+
+    private String isbn;
+
+    private LocalDate publicationDate;
+
+    private Integer pages;
+    private Author author;
+
+    /**
+     * @see com.faesa.librarycli.shared.core.adapters.AbstractSimpleJDBCRepository
+     * @deprecated Used only by AbstractSimpleJDBCRepository to instantiate objects from the database using reflection
+     */
+    @Deprecated(forRemoval = true)
+    protected Book() {
+    }
+
+    public Book(String title, String isbn, LocalDate publicationDate, Integer pages, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+        this.pages = pages;
+        this.author = author;
+    }
+
+    @Override
+    public void extract(PreparedStatement statement) {
+        try {
+            statement.setString(1, title);
+            statement.setString(2, isbn);
+            statement.setDate(3, java.sql.Date.valueOf(publicationDate));
+            statement.setInt(4, pages);
+            statement.setLong(5, author.getId());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void assignId(Long id) {
+        Assert.state(this.id == null, "Id already assigned");
+        this.id = id;
+    }
+
+    @Override
+    public void fromResultSet(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getLong("id");
+            this.title = resultSet.getString("title");
+            this.isbn = resultSet.getString("isbn");
+            this.publicationDate = resultSet.getDate("publication_date").toLocalDate();
+            this.pages = resultSet.getInt("pages");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/faesa/librarycli/core/createbook/CreateBook.java
+++ src/main/java/com/faesa/librarycli/core/createbook/CreateBook.java
@@ -1,16 +1,61 @@
 package com.faesa.librarycli.core.createbook;
 
+import com.faesa.librarycli.core.createauthor.AuthorRepository;
+import com.faesa.librarycli.shared.core.validators.UniqueValue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.ISBN;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @ShellComponent
 @RequiredArgsConstructor
 public class CreateBook {
 
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
 
     @ShellMethod(value = "Creates a new book in the library", key = "create-book")
-    public String createBook() {
+    public String createBook(
+            @ShellOption(
+                    help = "The title of the book",
+                    value = {"-t", "--title"}
+            ) @NotBlank String title,
+            @ShellOption(
+                    help = "The id of the author of the book",
+                    value = {"-a", "--author-id"}
+            ) @NotNull @Positive Long authorId,
+            @ShellOption(
+                    help = "The ISBN of the book",
+                    value = {"-i", "--isbn"}
+            ) @NotBlank @ISBN(type = ISBN.Type.ISBN_10) @UniqueValue(domainClass = Book.class, fieldName = "isbn") String isbn,
+            @ShellOption(
+                    help = "The publication date of the book",
+                    value = {"-d", "--publication-date"}
+            ) @NotBlank
+            @Pattern(regexp = "^(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[0-1])/(19|20)\\d{2}$")
+            String publicationDate,
+            @ShellOption(
+                    help = "The number of pages of the book",
+                    value = {"-p", "--pages"}
+            ) @NotNull @Positive Integer pages
+    ) {
+        var possibleAuthor = authorRepository.findById(authorId);
+        if (possibleAuthor.isEmpty()) {
+            return "Author not found";
+        }
+        var parsedPublicationDate = LocalDate.parse(publicationDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        var author = possibleAuthor.get();
+        var book = new Book(title, isbn, parsedPublicationDate, pages, author);
+        bookRepository.save(book);
         return "Book created successfully";
     }
 }
