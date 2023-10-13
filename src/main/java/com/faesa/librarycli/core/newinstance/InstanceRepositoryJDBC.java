f1dcb4038ccd5468a6f7f2b1128b7ca80b850508
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-13 19:52:10 -0300

    feat: Add functionality to register a new instance of a book in the library
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/newinstance/InstanceRepositoryJDBC.java
+++ src/main/java/com/faesa/librarycli/core/newinstance/InstanceRepositoryJDBC.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -15,7 +17,21 @@ public class InstanceRepositoryJDBC implements InstanceRepository {
 
     @Override
     public Instance save(Instance entity) {
-        return null;
+        String sql = "INSERT INTO instance (status, type, book_isbn) VALUES (?, ?, ?)";
+
+        try (var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            connection.setAutoCommit(false);
+            entity.setStatementValues(statement);
+            statement.executeUpdate();
+            var generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                entity.assignId(generatedKeys.getLong(1));
+            }
+            connection.commit();
+            return entity;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- src/main/java/com/faesa/librarycli/core/newinstance/NewInstance.java
+++ src/main/java/com/faesa/librarycli/core/newinstance/NewInstance.java
@@ -1,11 +1,72 @@
 package com.faesa.librarycli.core.newinstance;
 
+import com.faesa.librarycli.core.createbook.BookRepository;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.ISBN;
+import org.springframework.shell.CompletionContext;
+import org.springframework.shell.CompletionProposal;
 import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.springframework.shell.standard.ValueProvider;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @ShellComponent
 @RequiredArgsConstructor
 public class NewInstance {
 
     private final InstanceRepository instanceRepository;
+    private final BookRepository bookRepository;
+
+    @ShellMethod(key = "new-instance", value = "Register a new instance of a book on library")
+    public String handle(
+            @ShellOption(help = "The book's isbn", value = {"-i", "--isbn"})
+            @NotBlank
+            @ISBN(type = ISBN.Type.ISBN_10) String bookIsbn,
+            @ShellOption(
+                    help = "The instance's type, it must be either FREE or RESTRICTED",
+                    valueProvider = InstanceTypeValueProvider.class,
+                    value = {"-t", "--type"}
+            ) @NotBlank String type,
+            @ShellOption(
+                    help = "The instance's status, it must be either AVAILABLE or UNAVAILABLE",
+                    valueProvider = InstanceStatusValueProvider.class,
+                    value = {"-s", "--status"},
+                    defaultValue = "AVAILABLE"
+            ) @NotBlank String status
+    ) {
+        var instanceStatus = InstanceStatus.valueOf(status.toUpperCase());
+        var instanceType = InstanceType.valueOf(type.toUpperCase());
+        return bookRepository.findByIsbn(bookIsbn).map(book -> {
+            var instance = new Instance(instanceStatus, instanceType, book);
+            instanceRepository.save(instance);
+            return "Instance created!";
+        }).orElse("Book not found!");
+    }
+}
+
+@Component
+class InstanceTypeValueProvider implements ValueProvider {
+    @Override
+    public List<CompletionProposal> complete(CompletionContext completionContext) {
+        return Stream.of(InstanceType.values())
+                .map(Enum::name)
+                .map(CompletionProposal::new)
+                .toList();
+    }
+}
+
+@Component
+class InstanceStatusValueProvider implements ValueProvider {
+    @Override
+    public List<CompletionProposal> complete(CompletionContext completionContext) {
+        return Stream.of(InstanceStatus.values())
+                .map(Enum::name)
+                .map(CompletionProposal::new)
+                .toList();
+    }
 }
