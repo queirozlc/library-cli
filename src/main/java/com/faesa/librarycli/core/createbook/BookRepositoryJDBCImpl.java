b72177868b1e64af169526a019aecb707510144f
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-14 17:43:21 -0300

    [BookRepositoryJDBCImpl] Refactor SQL queries for finding books and instances
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/createbook/BookRepositoryJDBCImpl.java
+++ src/main/java/com/faesa/librarycli/core/createbook/BookRepositoryJDBCImpl.java
@@ -1,6 +1,9 @@
 package com.faesa.librarycli.core.createbook;
 
-import com.faesa.librarycli.core.createauthor.AuthorRepository;
+import com.faesa.librarycli.core.createauthor.Author;
+import com.faesa.librarycli.core.newinstance.Instance;
+import com.faesa.librarycli.core.newinstance.InstanceStatus;
+import com.faesa.librarycli.core.newinstance.InstanceType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +21,6 @@ import java.util.Optional;
 public class BookRepositoryJDBCImpl implements BookRepository {
 
     private final Connection connection;
-    private final AuthorRepository authorRepository;
 
     @Override
     public Book save(Book entity) {
@@ -37,12 +39,12 @@ public class BookRepositoryJDBCImpl implements BookRepository {
 
     @Override
     public Optional<Book> findById(Long id) {
-        String sql = "SELECT * FROM book WHERE id = ?";
+        String sql = getFindQueryWithPredicate("b.id = ?");
         try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
             var resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                return Optional.of(buildBook(resultSet));
+                return Optional.of(fromResultSet(resultSet));
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -64,13 +66,12 @@ public class BookRepositoryJDBCImpl implements BookRepository {
     @Override
     public Collection<Book> findAll() {
         var books = new ArrayList<>(Collections.<Book>emptyList());
-        String sql = "SELECT * FROM book";
+        String sql = getFindAllQuery();
+
         try (var statement = connection.prepareStatement(sql)) {
             var resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                var book = buildBook(resultSet);
-                book.assignId(resultSet.getLong("id"));
-                books.add(book);
+                books.add(fromResultSet(resultSet));
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -93,39 +94,82 @@ public class BookRepositoryJDBCImpl implements BookRepository {
         return false;
     }
 
-    private Book buildBook(ResultSet resultSet) throws SQLException {
-
-        var author = authorRepository
-                .findById(resultSet.getLong("author_id"))
-                .orElseThrow(() -> new RuntimeException("Author not found"));
-
-        var title = resultSet.getString("title");
-
-        var isbn = resultSet.getString("isbn");
-
-        var publicationDate = resultSet.getDate("publication_date").toLocalDate();
-
-        var pages = resultSet.getInt("pages");
-
-        return new Book(title, isbn, publicationDate, pages, author);
-    }
-
 
     @Override
     public Optional<Book> findByIsbn(String bookIsbn) {
-        String sql = "SELECT * FROM book WHERE isbn = ?";
+        String sql = getFindQueryWithPredicate("b.isbn = ?");
+
         try (var statement = connection.prepareStatement(sql)) {
             statement.setString(1, bookIsbn);
             var resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                var book = buildBook(resultSet);
-                book.assignId(resultSet.getLong("id"));
-                return Optional.of(book);
+                return Optional.of(fromResultSet(resultSet));
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         return Optional.empty();
+    }
+
+    private Book fromResultSet(ResultSet resultSet) {
+        try {
+            var author = new Author(
+                    resultSet.getString("name"),
+                    resultSet.getString("nationality")
+            );
+            author.assignId(resultSet.getLong("author_id"));
+            var book = new Book(
+                    resultSet.getString("title"),
+                    resultSet.getString("isbn"),
+                    resultSet.getDate("publication_date").toLocalDate(),
+                    resultSet.getInt("pages"),
+                    author
+            );
+            book.assignId(resultSet.getLong("id"));
+            this.getInstances(book);
+            return book;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getInstances(Book book) {
+        String sql = getInstancesQuery();
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getIsbn());
+            var resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                var instanceType = InstanceType.supports(resultSet.getString("type"));
+                var instanceStatus = InstanceStatus.valueOf(resultSet.getString("status"));
+                var instance = new Instance(instanceStatus, instanceType, book);
+                instance.assignId(resultSet.getLong("instance_id"));
+                book.addInstance(instance);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getFindQueryWithPredicate(String predicate) {
+        return "SELECT b.*, a.name, a.nationality " +
+                "FROM book b " +
+                "INNER JOIN author a ON b.author_id = a.id " +
+                "WHERE " + predicate + " " +
+                "ORDER BY b.title";
+
+    }
+
+    private String getFindAllQuery() {
+        return "SELECT b.* " +
+                "FROM book b " +
+                "INNER JOIN author a ON b.author_id = a.id " +
+                "ORDER BY b.title";
+    }
+
+    private String getInstancesQuery() {
+        return "SELECT i.id as instance_id, i.type, i.status " +
+                "FROM instance i " +
+                "WHERE i.book_isbn = ?";
     }
 }
