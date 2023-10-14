b2f744f1d21c74b91abadd042820b5c737792780
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-14 17:34:49 -0300

    Implement method to verify if an instance type can accept a hold or check-out

--- src/main/java/com/faesa/librarycli/core/newinstance/InstanceType.java
+++ src/main/java/com/faesa/librarycli/core/newinstance/InstanceType.java
@@ -1,11 +1,25 @@
 package com.faesa.librarycli.core.newinstance;
 
+import com.faesa.librarycli.core.registerpatron.Patron;
+
 public enum InstanceType {
 
-    FREE,
-    RESTRICTED;
+    FREE {
+        @Override
+        public boolean acceptsHold(Patron patron) {
+            return true;
+        }
+    },
+    RESTRICTED {
+        @Override
+        public boolean acceptsHold(Patron patron) {
+            return patron.canBorrowRestrictedInstances();
+        }
+    };
 
     public static InstanceType supports(String value) {
         return InstanceType.valueOf(value.toUpperCase());
     }
+
+    public abstract boolean acceptsHold(Patron patron);
 }
