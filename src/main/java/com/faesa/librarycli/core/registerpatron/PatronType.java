5f8fa65dad01aefdda05211195360ca5beae9f95
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-14 17:32:42 -0300

    Implement methods to check the hold duration and fee for each type

--- src/main/java/com/faesa/librarycli/core/registerpatron/PatronType.java
+++ src/main/java/com/faesa/librarycli/core/registerpatron/PatronType.java
@@ -1,11 +1,50 @@
 package com.faesa.librarycli.core.registerpatron;
 
+import java.math.BigDecimal;
+
 public enum PatronType {
-    STUDENT,
-    RESEARCHER,
-    REGULAR;
+    STUDENT {
+        @Override
+        public Integer availableHoldDuration() {
+            return 7;
+        }
+
+        @Override
+        public BigDecimal currentHoldFee() {
+            /// students by default pay the same as regular patrons, but this can be changed if some policy is implemented
+            return BigDecimal.valueOf(0.5);
+        }
+    },
+    RESEARCHER {
+        @Override
+        public Integer availableHoldDuration() {
+            return null;
+        }
+
+        @Override
+        public BigDecimal currentHoldFee() {
+            // research does not pay fees
+            return BigDecimal.ZERO;
+        }
+    },
+    REGULAR {
+        @Override
+        public Integer availableHoldDuration() {
+            return 7;
+        }
+
+        @Override
+        public BigDecimal currentHoldFee() {
+            // regular patrons pay 5% of the book price to place a hold
+            return BigDecimal.valueOf(0.5);
+        }
+    };
 
     public static PatronType supports(String type) {
         return PatronType.valueOf(type.toUpperCase());
     }
+
+    public abstract Integer availableHoldDuration();
+
+    public abstract BigDecimal currentHoldFee();
 }
