b5ec8722ab2fff1061c8b0c570fcd9c829281487
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-15 18:24:23 -0300

    Add validation and shell command to perform book checkout
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/checkoutbook/CheckoutBook.java
+++ src/main/java/com/faesa/librarycli/core/checkoutbook/CheckoutBook.java
@@ -1,2 +1,29 @@
-package com.faesa.librarycli.core.checkoutbook;public class CheckoutBook {
+package com.faesa.librarycli.core.checkoutbook;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+@RequiredArgsConstructor
+public class CheckoutBook {
+
+
+    @ShellMethod(value = "Lends a book for some patron", key = "checkout-book")
+    public String perform(
+            @ShellOption(value = {"-p", "--patron"}, help = "Patron's id")
+            @NotNull @Positive Long patronId,
+
+            @ShellOption(value = {"-b", "--hold"}, help = "Hold's id")
+            @NotNull @Positive Long holdId,
+
+            @ShellOption(value = {"-t", "--time"}, help = "Borrowing time in days")
+            @NotNull @Positive @Range(min = 1, max = 60) Integer time
+    ) {
+        return "";
+    }
 }
