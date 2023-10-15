7f3a964ef4bfadd1292ef0b2d3d552d6cf7696a2
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-15 18:22:30 -0300

    [Loan] Add constructor to initialize loan attributes and the current attributes for a loan
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/checkoutbook/Loan.java
+++ src/main/java/com/faesa/librarycli/core/checkoutbook/Loan.java
@@ -1,2 +1,36 @@
-package com.faesa.librarycli.core.checkoutbook;public class Loan {
+package com.faesa.librarycli.core.checkoutbook;
+
+import com.faesa.librarycli.core.placinghold.Hold;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class Loan {
+
+    private final Hold hold;
+    private Long id;
+    private Integer time;
+
+    private Instant loanDate;
+
+    private Instant returnDate;
+
+    private BigDecimal overdueFine;
+
+    /**
+     * <p>
+     * Loan's are built by held books, so the constructor receives a current hold.
+     * </p>
+     *
+     * @param hold The instance of the book that is being held.
+     * @param time The time in days that the book will be borrowed.
+     */
+    public Loan(Hold hold, Integer time) {
+        this.time = time;
+        this.hold = hold;
+        this.loanDate = Instant.now();
+        this.returnDate = Instant.now().plus(time, ChronoUnit.DAYS);
+        this.overdueFine = BigDecimal.ZERO;
+    }
 }
