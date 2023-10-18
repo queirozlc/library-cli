f13cc3c52c81bc76792c08706c86e304d12f02b7
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-17 22:57:18 -0300

    feat: Add confirmation prompt when canceling a hold
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/faesa/librarycli/core/cancelinghold/CancelingHold.java
+++ src/main/java/com/faesa/librarycli/core/cancelinghold/CancelingHold.java
@@ -5,6 +5,7 @@ import com.faesa.librarycli.core.newinstance.Instance;
 import com.faesa.librarycli.core.newinstance.InstanceRepository;
 import com.faesa.librarycli.core.placinghold.HoldRepository;
 import com.faesa.librarycli.core.registerpatron.PatronRepository;
+import com.faesa.librarycli.shared.infra.shell.InputReader;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.util.List;
 
 @ShellComponent
 @ShellCommandGroup("Patron Commands")
@@ -22,6 +25,7 @@ public class CancelingHold {
     private final HoldRepository holdRepository;
     private final BookRepository bookRepository;
     private final InstanceRepository instanceRepository;
+    private final InputReader inputReader;
 
     @ShellMethod(value = "Cancel a book hold", key = "cancel-hold")
     public String perform(
@@ -30,6 +34,16 @@ public class CancelingHold {
             @ShellOption(value = {"-b", "--book"}, help = "Book ID")
             @Positive @NotNull Long bookId
     ) {
+        String confirmation = "";
+        do {
+            confirmation = inputReader.promptWithOptions(
+                    "Are you sure you want to cancel this hold?",
+                    null,
+                    List.of("Yes", "No", "y", "n", "yes", "no")
+            );
+        } while (!confirmation.equalsIgnoreCase("yes") && !confirmation.equalsIgnoreCase("y") && !confirmation.equalsIgnoreCase("no") && !confirmation.equalsIgnoreCase("n"));
+
+
         return patronRepository.findById(patronId).map(patron -> bookRepository.findById(bookId).map(book -> {
             if (book.hasHoldFrom(patron)) {
                 var holdToCancel = patron.getHoldIdFor(book);
